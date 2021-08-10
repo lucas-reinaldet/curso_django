bb0adcc5e4869f26f82e5dae6003f8bbfb24236f
Lucas Reinaldet <reinaldet.lucas@gmail.com>
2021-08-10 11:54:48 -0300

    Novas atualizaçoes

--- website/views.py
+++ website/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'HTML', 'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', data)
